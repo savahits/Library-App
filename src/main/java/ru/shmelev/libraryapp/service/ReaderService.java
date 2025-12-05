ad72e28d90ad4151200d46a15fcb7ee0ec3c4b55
Savelii <savahits@gmail.com>
2025-12-06 00:34:01 +0300

    feat: create save method

--- src/main/java/ru/shmelev/libraryapp/service/ReaderService.java
+++ src/main/java/ru/shmelev/libraryapp/service/ReaderService.java
@@ -2,6 +2,8 @@ package ru.shmelev.libraryapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.shmelev.libraryapp.dto.request.SaveReaderRequest;
+import ru.shmelev.libraryapp.dto.responce.ReaderResponse;
 import ru.shmelev.libraryapp.entity.Reader;
 import ru.shmelev.libraryapp.repository.ReaderRepository;
 
@@ -24,5 +26,22 @@ public class ReaderService {
     public Reader findById(Long id) {
         return readerRepository.findById(id).orElse(null);
     }
+
+    public ReaderResponse save(SaveReaderRequest request) {
+        Reader reader = new Reader();
+        reader.setName(request.name());
+        reader.setEmail(request.email());
+        reader.setSurname(request.surname());
+
+        Reader saveReader = readerRepository.save(reader);
+
+        return new ReaderResponse(
+                saveReader.getId(),
+                saveReader.getName(),
+                saveReader.getSurname(),
+                saveReader.getEmail()
+        );
+    }
+
 
 }
