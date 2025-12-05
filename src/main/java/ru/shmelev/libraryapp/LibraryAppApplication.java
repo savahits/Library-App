2346c89498f97b717b9b418053a7a93eb7925bf2
Savelii <savahits@gmail.com>
2025-12-05 23:15:29 +0300

    feat: add @PropertySource("classpath:database.properties")

--- src/main/java/ru/shmelev/libraryapp/LibraryAppApplication.java
+++ src/main/java/ru/shmelev/libraryapp/LibraryAppApplication.java
@@ -2,7 +2,9 @@ package ru.shmelev.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:database.properties")
 @SpringBootApplication
 public class LibraryAppApplication {
 
