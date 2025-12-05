f8ea1e03b08a0adeef739aa216c0ef1915a1e3fd
Savelii <savahits@gmail.com>
2025-12-05 20:20:39 +0300

    initialize Spring REST app LibraryApp

---
+++ src/test/java/ru/shmelev/libraryapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shmelev.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
