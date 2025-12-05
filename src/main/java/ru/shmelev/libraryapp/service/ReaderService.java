2d70f41ee6af109d8b80bf50b446e746e449fe6f
Savelii <savahits@gmail.com>
2025-12-05 21:22:55 +0300

    feat: create ReaderService

---
+++ src/main/java/ru/shmelev/libraryapp/service/ReaderService.java
@@ -0,0 +1,24 @@
+package ru.shmelev.libraryapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.shmelev.libraryapp.entity.Reader;
+import ru.shmelev.libraryapp.repository.ReaderRepository;
+
+import java.util.List;
+
+@Service
+public class ReaderService {
+
+    private final ReaderRepository readerRepository;
+
+    @Autowired
+    public ReaderService(ReaderRepository readerRepository) {
+        this.readerRepository = readerRepository;
+    }
+
+    public List<Reader> findAll() {
+        return readerRepository.findAll();
+    }
+
+}
