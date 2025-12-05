d828dba66d2e8a04d5e3ac80ebbd55e0f26fdd13
Savelii <savahits@gmail.com>
2025-12-05 23:12:09 +0300

    feat: create ReaderController with api/readers mapping

---
+++ src/main/java/ru/shmelev/libraryapp/controller/ReaderController.java
@@ -0,0 +1,28 @@
+package ru.shmelev.libraryapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+import ru.shmelev.libraryapp.entity.Reader;
+import ru.shmelev.libraryapp.service.ReaderService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/readers")
+public class ReaderController {
+
+    ReaderService readerService;
+    @Autowired
+    public ReaderController(ReaderService readerService) {
+        this.readerService = readerService;
+    }
+
+    @GetMapping
+    public List<Reader> getReaders() {
+        return readerService.getAll();
+    }
+
+}
