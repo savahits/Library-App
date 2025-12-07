b121ad95e80cb2a751309d778380de2009370cbe
Savelii <savahits@gmail.com>
2025-12-07 21:09:36 +0300

    feat: create JPA entity Author

--- src/main/java/ru/shmelev/libraryapp/entity/Author.java
+++ src/main/java/ru/shmelev/libraryapp/entity/Author.java
@@ -1,4 +1,36 @@
 package ru.shmelev.libraryapp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "surname", nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;  // Используем LocalDate для даты в Java
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books;
+
+    public Author() {}
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
