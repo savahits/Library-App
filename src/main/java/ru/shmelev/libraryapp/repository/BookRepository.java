8c818386072561d88385f9877541186b6b1cd178
Savelii <savahits@gmail.com>
2025-12-07 21:10:23 +0300

    fix: add @Repository annotation to BookRepository

--- src/main/java/ru/shmelev/libraryapp/repository/BookRepository.java
+++ src/main/java/ru/shmelev/libraryapp/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package ru.shmelev.libraryapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.shmelev.libraryapp.entity.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
