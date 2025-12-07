4fe46c4b945dc5bdcc6061f61aacefe9a2d4d0c5
Savelii <savahits@gmail.com>
2025-12-07 21:08:20 +0300

    feat: create JPA entity Book

--- src/main/java/ru/shmelev/libraryapp/entity/Book.java
+++ src/main/java/ru/shmelev/libraryapp/entity/Book.java
@@ -1,4 +1,55 @@
 package ru.shmelev.libraryapp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString(exclude = "author")
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "isbn", nullable = false, unique = true, length = 20)
+    private String isbn;
+
+    @Column(name = "publication_year")
+    private Integer publicationYear;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "author_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "book_author_id_fkey")
+    )
+    private Author author;
+
+    public Book() {}
+
+    public Book(String title, String isbn, Integer publicationYear) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id != null && id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
