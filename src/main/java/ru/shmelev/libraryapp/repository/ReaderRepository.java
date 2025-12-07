40c0ff581c54783d084ace157ae29b2acfe655d7
Savelii <savahits@gmail.com>
2025-12-07 21:37:53 +0300

    feat: add existsByEmail and findByEmail methods to ReaderRepository

--- src/main/java/ru/shmelev/libraryapp/repository/ReaderRepository.java
+++ src/main/java/ru/shmelev/libraryapp/repository/ReaderRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.libraryapp.entity.Reader;
 
+import java.util.Optional;
+
 @Repository
 public interface ReaderRepository extends JpaRepository<Reader, Long> {
+    boolean existsByEmail(String email);
 
+    Optional<Reader> findByEmail(String email);
 }
