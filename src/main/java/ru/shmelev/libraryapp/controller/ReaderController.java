a7ba2da1031b4d5bde273c4821f991adfdce05bb
Savelii <savahits@gmail.com>
2025-12-08 01:15:36 +0300

    refactor: delete useless import

--- src/main/java/ru/shmelev/libraryapp/controller/ReaderController.java
+++ src/main/java/ru/shmelev/libraryapp/controller/ReaderController.java
@@ -2,6 +2,7 @@ package ru.shmelev.libraryapp.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.shmelev.libraryapp.dto.request.SaveReaderRequest;
