f1181c28a749b646e0e31bb4c2e1d17f087fc8e0
Savva <savahits@gmail.com>
2025-12-09 02:00:51 +0300

    refactor: add some validators for DTO SaveReaderRequest

--- src/main/java/ru/shmelev/libraryapp/dto/request/SaveReaderRequest.java
+++ src/main/java/ru/shmelev/libraryapp/dto/request/SaveReaderRequest.java
@@ -3,6 +3,7 @@ package ru.shmelev.libraryapp.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SaveReaderRequest(
         @NotBlank(message = "Имя обязательно")
@@ -10,9 +11,11 @@ public record SaveReaderRequest(
         String name,
 
         @JsonProperty("surname")
+        @NotBlank(message = "Фамилия обязательно")
         String surname,
 
         @Email(message = "Некорректный email")
         @JsonProperty("email")
+        @NotNull(message = "Email обязателен")
         String email
 ) {}
