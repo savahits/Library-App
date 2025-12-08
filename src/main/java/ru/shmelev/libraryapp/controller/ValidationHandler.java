84749f72f73a7dd5bf844f7cade95e2e913bdbb2
Savva <savahits@gmail.com>
2025-12-09 01:59:49 +0300

    feat: create ValidationHandler

---
+++ src/main/java/ru/shmelev/libraryapp/controller/ValidationHandler.java
@@ -0,0 +1,33 @@
+package ru.shmelev.libraryapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Ошибка валидации");
+        response.put("errors", errors);
+
+        return response;
+    }
+}
