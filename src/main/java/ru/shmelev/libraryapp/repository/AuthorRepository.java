56da78c2316e66675fa366873551cf2dfeae37bd
Savva <savahits@gmail.com>
2025-12-12 01:48:49 +0300

    feat: add searchByFullName query method for AuthorRepository

--- src/main/java/ru/shmelev/libraryapp/repository/AuthorRepository.java
+++ src/main/java/ru/shmelev/libraryapp/repository/AuthorRepository.java
@@ -1,9 +1,19 @@
 package ru.shmelev.libraryapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.libraryapp.entity.Author;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    // query for text search
+    @Query("SELECT a FROM Author a WHERE " +
+            "LOWER(CONCAT(a.name, ' ', a.surname)) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "LOWER(CONCAT(a.surname, ' ', a.name)) LIKE LOWER(CONCAT('%', :search, '%'))")
+    List<Author> searchByFullName(@Param("search") String search);
 }
