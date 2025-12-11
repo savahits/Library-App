788dd3f02c4e42ee364631d83f98b1cfc62a156d
Savva <savahits@gmail.com>
2025-12-12 01:26:51 +0300

    feat: add deleteAuthor delete mapping method and add response statuses for methods

--- src/main/java/ru/shmelev/libraryapp/controller/AuthorController.java
+++ src/main/java/ru/shmelev/libraryapp/controller/AuthorController.java
@@ -2,6 +2,7 @@ package ru.shmelev.libraryapp.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.shmelev.libraryapp.dto.request.SaveAuthorRequest;
 import ru.shmelev.libraryapp.dto.responce.AuthorResponse;
@@ -21,13 +22,21 @@ public class AuthorController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<AuthorResponse> getAuthors() {
         return authorService.findAll();
     }
 
     @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
     public AuthorResponse save(@RequestBody @Valid SaveAuthorRequest request) {
         return authorService.save(request);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAuthor(@RequestParam Long id) {
+        authorService.delete(id);
     }
 
 }
