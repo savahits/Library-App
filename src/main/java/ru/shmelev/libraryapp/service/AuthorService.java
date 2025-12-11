43a3472300556b792f0c7687f413d7df1f29cd18
Savva <savahits@gmail.com>
2025-12-11 11:25:14 +0300

    feat: add toResponse DTO method and findAll method for AuthorService

--- src/main/java/ru/shmelev/libraryapp/service/AuthorService.java
+++ src/main/java/ru/shmelev/libraryapp/service/AuthorService.java
@@ -1,8 +1,13 @@
 package ru.shmelev.libraryapp.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.shmelev.libraryapp.dto.responce.AuthorResponse;
+import ru.shmelev.libraryapp.entity.Author;
 import ru.shmelev.libraryapp.repository.AuthorRepository;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
@@ -12,6 +17,22 @@ public class AuthorService {
     @Autowired
     public AuthorService(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+    @Transactional()
+    public List<AuthorResponse> findAll() {
+        return authorRepository.findAll().stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    private AuthorResponse toResponse(Author author) {
+        return new AuthorResponse(
+                author.getId(),
+                author.getName(),
+                author.getSurname(),
+                author.getBirthDate()
+        );
     }
 
 
