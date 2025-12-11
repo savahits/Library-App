70d65ef087b427258f5038d0374091a2e522510c
Savva <savahits@gmail.com>
2025-12-12 02:30:17 +0300

    refactor: upgrade searchByFullName method for AuthorRepository

--- src/main/java/ru/shmelev/libraryapp/repository/AuthorRepository.java
+++ src/main/java/ru/shmelev/libraryapp/repository/AuthorRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.shmelev.libraryapp.dto.response.AuthorSearchResponse;
 import ru.shmelev.libraryapp.entity.Author;
 
 import java.util.List;
@@ -12,8 +13,16 @@ import java.util.List;
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     // query for text search
-    @Query("SELECT a FROM Author a WHERE " +
-            "LOWER(CONCAT(a.name, ' ', a.surname)) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
-            "LOWER(CONCAT(a.surname, ' ', a.name)) LIKE LOWER(CONCAT('%', :search, '%'))")
-    List<Author> searchByFullName(@Param("search") String search);
+    @Query("""
+        SELECT new ru.shmelev.libraryapp.dto.response.AuthorSearchResponse(
+            a.id, 
+            a.name, 
+            a.surname, 
+            a.birthDate
+        ) 
+        FROM Author a 
+        WHERE LOWER(CONCAT(a.name, ' ', a.surname)) LIKE LOWER(CONCAT('%', :search, '%')) 
+           OR LOWER(CONCAT(a.surname, ' ', a.name)) LIKE LOWER(CONCAT('%', :search, '%'))
+        """)
+    List<AuthorSearchResponse> searchByFullName(@Param("search") String search);
 }
